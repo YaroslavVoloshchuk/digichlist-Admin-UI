0107cdbbbbd1396cd164bf82a2619dcd1ece63d6
Yaroslav Voloshchuk <75930857+YaroslavVoloshchuk@users.noreply.github.com>
2021-05-25 16:24:54 +0300

    Update environment.tsx

--- src/environments/environment.tsx
+++ src/environments/environment.tsx
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    //BASEURL: 'https://demo.ifdevops.store:5000/api/',
-    BASEURL: 'http://192.168.0.103:5000/api/',
+    BASEURL: 'https://demo.ifdevops.store:5000/api/',
+    //BASEURL: 'http://192.168.0.103:5000/api/',
 };
